67e737f0cbb11dca16fc89f0c44b04a860c0abf6
Dongwoo-Kim <dongeu47@gmail.com>
2023-04-13 20:15:20 +0900

    cicd test
    cicd test

--- test-app/src/app.controller.spec.ts
+++ test-app/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "200"', () => {
-      expect(appController.getHello()).toBe(200);
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
